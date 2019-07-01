f4690dde8a9772a684cdcf34df8694dd502e4939
Jim <jim_king_2000@163.com>
2019-07-01 13:36:46 +0800

    refactory

--- components/TrackPlayer/ButtonPlayOrPause.js
+++ components/TrackPlayer/ButtonPlayOrPause.js
@@ -1,20 +1,12 @@
-import React, { Component } from 'react';
 import { Button } from 'grommet';
 import { Pause, Play } from 'grommet-icons';
 
-export default class extends Component {
-  onPlayOrPause = () => {
-    if (this.props.isPlay) this.props.onPlay();
-    else this.props.onPause();
-  }
-
-  render() {
-    return <Button
-      margin='xsmall'
-      plain={false}
-      disabled={this.props.disabled}
-      icon={this.props.isPlay ? <Play /> : <Pause />}
-      onClick={this.onPlayOrPause}
-      name='onPlayOrPause' />;
-  }
-}
+export default ({ disabled, isPlay, onPlay, onPause }) => (
+  <Button
+    margin='xsmall'
+    plain={false}
+    disabled={disabled}
+    icon={isPlay ? <Play /> : <Pause />}
+    onClick={isPlay ? onPlay : onPause}
+    name='onPlayOrPause' />
+);
