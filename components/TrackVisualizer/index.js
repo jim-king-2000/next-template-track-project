213168e7f5f8852851113c76f32a553f7b2048d1
Jim <jim_king_2000@163.com>
2019-07-02 00:31:49 +0800

    refactory

--- components/TrackVisualizer/index.js
+++ components/TrackVisualizer/index.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
 import { Box, Stack, Text } from 'grommet';
 import { observer } from 'mobx-react';
 import moment from 'moment';
 
-export function formatTime2(timestamp) {
-  return timestamp && (
+function formatTime2(timestamp) {
+  return (
     <>
       {moment(timestamp).format('YY/MM/DD')}
       <br />
@@ -13,64 +12,70 @@ export function formatTime2(timestamp) {
   );
 }
 
-@observer
-export class TrackVisualizer extends Component {
-  constructor(props) {
-    super(props);
-  }
+const ClockItem = ({ timestamp, current }) => (
+  <Box>
+    <Text color={current ? 'brand' : undefined}>
+      {moment(timestamp).format('YY/MM/DD')}
+      <br />
+      {moment(timestamp).format('HH:mm:ss')}
+    </Text>
+  </Box>
+);
 
-  render() {
-    const visualData = this.props.visualization;
-    const player = this.props.timeline;
-    let curPos = (player.currentTimestamp - player.startTimestamp) /
-      (player.endTimestamp - player.startTimestamp);
-    curPos = Math.round(curPos * 10000) / 100 + '%';
-    return (
-      <Box margin='xsmall' gap='xsmall' flex={false}>
-        {visualData && visualData.length > 0 && 
-          <div style={{ maxHeight: '150px', overflow: 'auto' }}>
-          <Box flex={false} margin='xsmall'>
-            {visualData.map(trackVisual => (
-              <Box key={trackVisual.thingId}>
-                <Text>{trackVisual.name}</Text>
-                <Stack margin={{ right: '6px' }} height='8px'>
-                  <Box
-                    margin={{ top: '2px' }}
-                    height='4px'
-                    background='light-6'>
-                  </Box>
-                  {trackVisual.visualData && trackVisual.visualData.map(
-                    (seg, i) => (
-                      <Box
-                        key={i}
-                        height='4px'
-                        margin={{ left: seg.margin, top: '2px' }}
-                        width={seg.width}
-                        background='green' />)
-                  )}
-                  <Box
-                    height='8px'
-                    width='4px'
-                    margin={{ left: curPos }}
-                    background='brand' />
-                </Stack>
-              </Box>
-            ))}
-          </Box>
-        </div>}
-        {player.endTimestamp - player.startTimestamp > 0 &&
-          <Box direction='row' justify='between' flex={false}>
-            <Box>
-              <Text>{formatTime2(player.startTimestamp)}</Text>
-            </Box>
-            <Box>
-              <Text color='brand'>{formatTime2(player.currentTimestamp)}</Text>
-            </Box>
-            <Box>
-              <Text>{formatTime2(player.endTimestamp)}</Text>
-            </Box>
-          </Box>}
+const ClockRange = observer(({ timeline }) => (
+  <>
+    {timeline.endTimestamp - timeline.startTimestamp > 0 &&
+      <Box direction='row' justify='between' flex={false}>
+        <ClockItem timestamp={timeline.startTimestamp} />
+        <ClockItem timestamp={timeline.currentTimestamp} current />
+        <ClockItem timestamp={timeline.endTimestamp} />
       </Box>
-    );
-  }
+    }
+  </>
+));
+
+function calcCurrent(timeline) {
+  const curPos = (timeline.currentTimestamp - timeline.startTimestamp) /
+  (timeline.endTimestamp - timeline.startTimestamp);
+  return Math.round(curPos * 10000) / 100 + '%';
 }
+
+const VisualItem = observer(({ timeline, trackVisual }) => (
+  <Box key={trackVisual.thingId}>
+    <Text>{trackVisual.name}</Text>
+    <Stack margin={{ right: '6px' }} height='8px'>
+      <Box
+        margin={{ top: '2px' }}
+        height='4px'
+        background='light-6'>
+      </Box>
+      {trackVisual.visualData && trackVisual.visualData.map(
+        (seg, i) => (
+          <Box
+            key={i}
+            height='4px'
+            margin={{ left: seg.margin, top: '2px' }}
+            width={seg.width}
+            background='brand' />)
+      )}
+      <Box
+        height='8px'
+        width='4px'
+        margin={{ left: calcCurrent(timeline) }}
+        background='brand' />
+    </Stack>
+  </Box>
+));
+
+export const TrackVisualizer = observer(({ timeline, visualization }) => (
+  <Box margin='xsmall' gap='xsmall' flex={false}>
+    <div style={{ maxHeight: '150px', overflow: 'auto' }}>
+      <Box flex={false} margin='xsmall'>
+        {visualization.map(trackVisual => <VisualItem
+          trackVisual={trackVisual}
+          timeline={timeline} />)}
+      </Box>
+    </div>
+    <ClockRange timeline={timeline} />
+  </Box>
+));
