9371553d823c3110f35027fa4241887c39763f2c
Jim <jim_king_2000@163.com>
2019-07-01 14:05:53 +0800

    refactory

--- components/MapCanvas.js
+++ components/MapCanvas.js
@@ -3,15 +3,11 @@ import { Box } from 'grommet';
 import { CanvasContainer, CanvasTracks,
   CanvasPositions } from 'location-backbone-canvas';
 
-export default observer(({ store }) => {
-  // console.log(store.trackPlayerStore.get().playerTimeline)
-  const things = store.trackPlayerStore.get().things;
-  return (
+export default observer(({ store }) => (
     <Box flex={{ grow: 1, shrink: 1 }}>
       <CanvasContainer mapkey='99c0746b70009d496380367b4f8f8494'>
         <CanvasTracks tracks={store.tracks.get()} />
-        <CanvasPositions things={things} />
+        <CanvasPositions things={store.trackPlayerStore.get().things} />
       </CanvasContainer>
     </Box>
-  );
-});
+  ));
